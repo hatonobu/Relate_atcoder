459eabd803d06e65d1fef4f534a5c9717b35b662
hatonobu <moyayoshida7230my@gmail.com>
2023-06-20 04:41:10 +0900

    pre commit

---
+++ Atcoder_templete_solve/no1.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque,defaultdict
+import itertools
+import heapq
+import bisect
+import queue
+
+#sys.setrecursionlimit(10 ** 9)
+input = lambda: sys.stdin.readline().rstrip()
+ii = lambda: int(input())
+mi = lambda: map(int, input().split())
+li = lambda: list(mi())
+lli = lambda n: [li() for _ in range(n)]
+mod = 998244353
