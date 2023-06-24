e1d8872b5c24a440b55f6bb6efec33211bc2bdd6
hatonobu <moyayoshida7230my@gmail.com>
2023-06-24 20:48:47 +0900

    one time

--- practice.py
+++ practice.py
@@ -13,4 +13,23 @@ li = lambda: list(mi())
 lli = lambda n: [li() for _ in range(n)]
 mod = 998244353
 
-N = ii()
+N,M = mi()
+AB = lli(M)
+CD = lli(M)
+d1 = defaultdict(list)
+d2 = defaultdict(list)
+ 
+for x,y in AB:
+    d1[x-1].append(y-1)
+    d1[y-1].append(x-1)
+for x,y in CD:
+    d2[x-1].append(y-1)
+    d2[y-1].append(x-1)
+ 
+print(d1,d2)
+
+ans = False
+if ans:
+    print("Yes")
+else:
+    print("No")
