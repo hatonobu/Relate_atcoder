912026c769579ad01811f614beb23e994d8d0bfe
hatonobu <moyayoshida7230my@gmail.com>
2023-08-12 01:47:43 +0900

    library

--- ABC_contest/ABC313/B.py
+++ ABC_contest/ABC313/B.py
@@ -4,7 +4,7 @@ import itertools
 import heapq
 import bisect
 import queue
-
+ 
 #sys.setrecursionlimit(10 ** 9)
 input = lambda: sys.stdin.readline().rstrip()
 ii = lambda: int(input())
@@ -13,18 +13,33 @@ li = lambda: list(mi())
 li_st = lambda: list(map(str, input().split()))
 lli = lambda n: [li() for _ in range(n)]
 mod = 998244353
-
+ 
 N,M = mi()
-
-s = set(range(1,N+1))
-
+ 
+d = defaultdict(list)
+ 
 for _ in range(M):
     a,b = mi()
-    s.discard(b)
-
-if len(s) == 1:
-    print(s.pop())
-else:
-    print(-1)
-
+    d[a].append(b)
+ 
+def dfs(ans,num):
+    used[num] = True
+    lis = d[num]
+    for n in lis:
+        if used[n] == False:
+            dfs(ans,n)
+ 
+for i in range(1,N+1):
+    times = 0
+    used = [False]*(N+3)
+    dfs(i,i)
+    for j in range(N+1):
+        if used[j] == True:
+            times += 1
+    if times == N:
+        print(i)
+        exit()
+ 
+print(-1)    
+ 
 
