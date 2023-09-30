19d382ce3a0b9c35b7ed504cd69ea94ffe69ddf9
hatonobu <moyayoshida7230my@gmail.com>
2023-09-30 20:16:58 +0900

    test

--- ABC_contest/sample.py
+++ ABC_contest/sample.py
@@ -16,31 +16,9 @@ li_st = lambda: list(map(str, input().split()))
 lli = lambda n: [li() for _ in range(n)]
 mod = 998244353
 
-N = ii()
-A = lli(2*N - 1)
-ans = 0
+N,M = mi()
+A = lli(M)
 
-def dfs(used,B):
-    global ans
-    if all(used):
-        ans = max(ans,B)
-        return True
-    a = used.index(False)
-    used[a] = True
-    for num in range(a+1,2*N):
-        if used[num] == False:
-            B_next = B ^ A[a][num-a-1]
-            #print(A[a][num-a-1])
-            used[num] = True
-            dfs(used,B_next)
-            used[num] = False
-    used[a] = False
-    return B
-
-used = [False] * (2*N)
-aa = dfs(used,0)
-print(ans)
-
-
+G = [[]*N]
 
 
